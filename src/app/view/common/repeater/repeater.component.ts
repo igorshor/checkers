6003b4e21f7e0532f31d9db7ff6371a8bf56b4d0
Igor Shor <ishor@varonis.com>
2018-05-03 17:28:39 +0300

    web components structure

--- src/app/view/common/repeater/repeater.component.ts
+++ src/app/view/common/repeater/repeater.component.ts
@@ -1,16 +1,49 @@
 import { buildHtmlTemplate } from "../../helpers/html-template-builder";
 
 export const name = 'repeat';
+
+export interface IRepeaterProps{
+    elements:string;
+}
+
+export interface IRepeaterState{
+    element:string[];
+}
+
 export class Repeater extends HTMLElement {
-    private template: HTMLElement;
+    private _template: HTMLElement;
     constructor() {
         super();
 
-        this.template = buildHtmlTemplate(name, require('./repeater.template.html'));
+        //this._template = buildHtmlTemplate(name, require('./repeater.template.html'));
         const element = document.getElementById('item');
         const strData = element.getAttribute('data-array');
         const data = JSON.parse(strData);
 
+    }
+
+    static get observedRefAttributes(): string[] {
+        return ['data'];
+    }
+
+    static get observedValueAttributes(): string[] {
+        return [];
+    }
+
+    static get observedAttributes() {
+        return [...Repeater.observedRefAttributes, ...Repeater.observedValueAttributes];
+    }
+
+    attributeChangedCallback(attrName: string, oldVal: any, newVal: any) {
+        if (oldVal === newVal) {
+            return;
+        }
+        if(Repeater.observedRefAttributes.indexOf(attrName) >= 0){
+
+        } else {
+
+        }
+        const data = JSON.parse(newVal);
     }
 }
 
