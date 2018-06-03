6355687e1e6944fd8e8c5c5faf9c650be7631fa0
Igor Shor <igors@codevalue.net>
2018-06-03 17:16:42 +0300

    test

--- src/app/view-model/models/game-event.ts
+++ src/app/view-model/models/game-event.ts
@@ -3,6 +3,6 @@ import { GameStage } from "../../model/common/game/game-stage";
 
 export class GameEvent {
     constructor(public state: GameStage, public winner?: PlayerEvent, public draw?: boolean) {
-
+ 
     }
 }
