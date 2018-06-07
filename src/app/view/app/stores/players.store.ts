dbdd701e9e48554c0295c47245b3319a93270a5e
Igor Shor <igors@codevalue.net>
2018-06-07 14:51:48 +0300

    start implementing players store

--- src/app/view/app/stores/players.store.ts
+++ src/app/view/app/stores/players.store.ts
@@ -1,3 +1,20 @@
-export class PlayersStore {
+import { Player } from "../../models/player.model";
+import { observable } from "mobx";
+import { ViewModel } from "../../../view-model/view-model";
+import { ChangeEvent } from "../../../view-model/models/change-event";
 
+export class PlayersStore {
+    public players: { [id: string]: Player };
+    @observable currentPlayer: Player;
+    constructor(private _viewModel: ViewModel) {
+        this.init();
+
+        _viewModel.change.subscribe((changeEvent: ChangeEvent) => {
+            this.currentPlayer = this.players[changeEvent.playerId.id];
+        });
+    }
+
+    private init() {
+
+    }
 }
